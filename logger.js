541b0422ec8b91c83e6890090c2e22c00d31f0be
amcmax1 <95625051+amcmax1@users.noreply.github.com>
2022-04-20 19:17:12 +0300

    ci(logging): docker config, packages and logger added

---
+++ logger.js
@@ -0,0 +1,9 @@
+// logger.js
+const pino = require("pino");
+
+// Create a logging instance
+const logger = pino({
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+});
+
+module.exports.logger = logger;
